46504c0e345fcbb64891a3ad243c559c21e2c02d
robmllze <86869208+robmllze@users.noreply.github.com>
2024-08-22 16:03:51 +1000

    chore: Add parseArgs function

---
+++ lib/src/parse_args.dart
@@ -0,0 +1,25 @@
+//.title
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//
+// Dart/Flutter (DF) Packages by DevCetra.com & contributors. Use of this
+// source code is governed by an an MIT-style license that can be found in the
+// LICENSE file located in this project's root directory.
+//
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//.title~
+
+Map<String, List<String>> parseArgs(List<String> args) {
+  final argsMap = <String, List<String>>{};
+  String? currentKey;
+
+  for (final arg in args) {
+    if (arg.startsWith('-')) {
+      currentKey = arg;
+      argsMap[currentKey] = [];
+    } else if (currentKey != null) {
+      argsMap[currentKey]!.add(arg);
+    }
+  }
+
+  return argsMap;
+}
